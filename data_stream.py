ba1b28ebad704fe56f013855928f6dba15518f80
heesters-nick <nick.heesters@epfl.ch>
2024-06-17 10:39:22 -0400

    seconds attempt to push from narval

--- data_stream.py
+++ data_stream.py
@@ -137,7 +137,7 @@ show_plot = False
 # Save plot
 save_plot = True
 
-platform = 'narval'  #'CANFAR'
+platform = 'narvall'  #'CANFAR'
 if platform == 'CANFAR':
     root_dir_main = '/arc/home/ashley/SSL/git/'
     root_dir_data = '/arc/projects/unions/'
