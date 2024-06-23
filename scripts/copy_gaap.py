1d715a4b6fc7df79c02dd0b89d538376f29302d7
heesters-nick <nick.heesters@epfl.ch>
2024-06-23 04:05:40 -0400

    update copy_gaap.py

--- scripts/copy_gaap.py
+++ scripts/copy_gaap.py
@@ -1,10 +1,12 @@
 import logging
 import os
 import subprocess
+import sys
 
 import vos
 from tqdm import tqdm
 
+sys.path.append('/home/heesters/projects/def-sfabbro/heesters/github/TileSlicer')
 from data_utils import setup_logging
 
 
@@ -16,30 +18,33 @@ def list_vospace(directory):
         return entry_list
     except subprocess.CalledProcessError as e:
         logging.error(f'Error listing {directory}: {e}')
-        return None
+        return []
 
 
-def find_catalogs(client, entries, vos_path, local_path):
+def find_catalogs(client, entries, vos_path, local_path, file_pattern):
     for entry in tqdm(entries):
         logging.debug(f'Processing entry: {entry}')
         sub_dir_path = os.path.join(vos_path, entry)
         if client.isdir(sub_dir_path):
+            local_file_path = os.path.join(sub_dir_path, entry, entry + file_pattern)
+            if os.path.exists(local_file_path):
+                logging.info(f'Catalog {entry} already downloaded. Skipping.')
+                continue
             files = list_vospace(sub_dir_path)
             logging.debug(f'Found files: {files}')
-            if files is not None:
-                for file in files:
-                    logging.debug(f'Processing file: {file}')
-                    file_path = os.path.join(sub_dir_path, file)
-                    if file.endswith('_ugriz_photoz_ext.cat'):
-                        # create the local directory if it doesn't exist to maintian the same structure
-                        os.makedirs(os.path.join(local_path, entry), exist_ok=True)
-                        local_file_path = os.path.join(local_path, entry, file)
-                        if os.path.exists(local_file_path):
-                            logging.info(f'File {file} already downloaded. Skipping.')
-                            continue
-                        if client.isfile(file_path):
-                            logging.info(f'Found catalog: {file}, starting download..')
-                            download_file(file_path, local_file_path)
+            for file in files:
+                logging.debug(f'Processing file: {file}')
+                file_path = os.path.join(sub_dir_path, file)
+                if file.endswith(file_pattern):
+                    # create the local directory if it doesn't exist to maintian the same structure
+                    os.makedirs(os.path.join(local_path, entry), exist_ok=True)
+                    local_file_path = os.path.join(local_path, entry, file)
+                    if os.path.exists(local_file_path):
+                        logging.info(f'File {file} already downloaded. Skipping.')
+                        continue
+                    if client.isfile(file_path):
+                        logging.info(f'Found catalog: {file}, starting download..')
+                        download_file(file_path, local_file_path)
 
 
 def download_file(vospace_path, local_path):
@@ -67,13 +72,15 @@ def main():
 
     # List the contents of the base directory recursively
     entries = list_vospace(base_vospace_dir)
+    file_pattern = '_ugriz_photoz_ext.cat'
+
     if entries is not None:
         logging.info(f'Found {len(entries)} entries in the directory.')
         print(entries[:30])
 
     if entries:
         # Process the directory entries
-        find_catalogs(client, entries, base_vospace_dir, local_dir)
+        find_catalogs(client, entries, base_vospace_dir, local_dir, file_pattern)
 
 
 if __name__ == '__main__':
